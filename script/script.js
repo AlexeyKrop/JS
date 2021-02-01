6b410bb477e4bd6e43e83b93a9905b857d021c92
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-01 13:14:27 +0300

    Сделал задание по lesson03

--- script/script.js
+++ script/script.js
@@ -1,15 +1,34 @@
-let money = 1e5;
+let money = +prompt('Ваш месячный доход?', 0);
 let income = 'фриланс';
-let addExpenses = 'РАСХОДЫ';
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 1e6;
 let period = 12;
 console.log(typeof(money));
 console.log(typeof(income));
 console.log(typeof(deposit));
 console.log(addExpenses.length);
-console.log(addExpenses.split(', '));
+console.log(addExpenses.split());
+console.log('Цель заработать: ' + mission + ' рублей');
 console.log('Период равен ' + period + ' месяцев' );
 console.log(addExpenses.toLowerCase());
-let budgetDay = money / 30;
-console.log(budgetDay);
+let budgetDay = Math.floor(money / 30);
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?');
+let budgetMonth = money - (amount1 + amount2);
+console.log('Бюджет на месяц: ' + budgetMonth);
+let monthToMission = Math.ceil(mission / budgetMonth);
+console.log('Цель будет достигнута за: ' + monthToMission + ' месяцев(-а)');
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('Бюджет на день: ' + budgetDay + 'рублей');
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay >= 0 && budgetDay < 600) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
+  console.log('Что то пошло не так');
+}
